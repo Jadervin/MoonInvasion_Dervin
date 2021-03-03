7bd8568d9af26df9bb92737036889dcacfb25a7c
Jonathan Dervin <jdervin@ltu.edu>
2021-03-03 13:02:38 -0500

    Error in Enemy Movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,15 +6,17 @@ public class EnemyMovement : MonoBehaviour
 {
     [Range(0, 100)]
     public float Speed;
-    private Vector3 movement = new Vector3(0, 0, 1);
-    [SerializeField]
-    public GameObject Enemy;
-    public Rigidbody rb;
-    public CharacterController controller;
-    Vector3 direction;
+    //private Vector3 movement;
+    //[SerializeField]
+    //public GameObject Enemy;
 
-    //public string movementAxis = "Horizontal";
-    //public string movementAxis2 = "Vertical";
+    //public Rigidbody rb;
+
+
+    public CharacterController controller;
+    //Vector3 direction;
+
+    
 
     //public string youwin;
 
@@ -31,32 +33,17 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
+        Vector3 movementVector = transform.forward * Speed * Time.deltaTime;
+
+        controller.Move(movementVector);
+        */
+        
 
         transform.position += transform.forward * Speed * Time.deltaTime;
 
 
-        /*
-        float movZ = direction.z * Speed * Time.deltaTime;
         
-        float gravity = Physics.gravity.y * Time.deltaTime;
-        
-
-       Vector3 movementVector = new Vector3(0, gravity, movZ);
-        
-
-
-        controller.Move(movementVector);
-        //Enemy.transform.forward(axis * Speed * Time.deltaTime, 0, 0));
-
-        
-        //float axis = Input.GetAxis(movementAxis);
-
-        
-
-        //float axis2 = Input.GetAxis(movementAxis2);
-
-        //Player.transform.Translate(new Vector3(0,0, axis2 * Speed * Time.deltaTime));
-        */
     }
     private void OnCollisionEnter(Collision collision)
     {
