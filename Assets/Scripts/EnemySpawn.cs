f775132c1e59ffc758852198bd32b0b215957d3f
Jonathan Dervin <jdervin@ltu.edu>
2021-03-08 20:52:27 -0500

    Multiple Spawners

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class EnemySpawn : MonoBehaviour
 {
     public Transform spawnPosition;
-    //float minSpawnSec = 2, maxSpawnSec = 10;
+    float minSpawnSec = 2, maxSpawnSec = 11;
     public GameObject enemyPrefab;
 
 
@@ -23,7 +23,7 @@ public class EnemySpawn : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        //spawnEverySeconds = Random.Range(minSpawnSec, maxSpawnSec);
+        spawnEverySeconds = Random.Range(minSpawnSec, maxSpawnSec);
     }
 
     // Update is called once per frame
