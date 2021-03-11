ddfc1b96cf90f0a8a2097dd5825495472c4e2350
Jonathan Dervin <jdervin@ltu.edu>
2021-03-11 16:59:53 -0500

    Button Timer and fixing Graphics

---
+++ Assets/Scripts/ButtonCooldown.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCooldown : MonoBehaviour
+{
+    [SerializeField]
+    Button myButton;
+    [SerializeField]
+    float cooldownDuration = 5;
+
+    void Awake()
+    {
+        // Get a reference to your button
+        myButton = GetComponent<Button>();
+
+        if (myButton != null)
+        {
+            
+            myButton.onClick.AddListener(OnButtonClick);
+        }
+    }
+
+
+    private void Start()
+    {
+        
+    }
+    // This method is called whenever myButton is pressed
+    void OnButtonClick()
+    {
+        StartCoroutine(Cooldown());
+    }
+
+    // Coroutine that will deactivate and reactivate the button 
+    IEnumerator Cooldown()
+    {
+        // Deactivate myButton
+        myButton.interactable = false;
+        // Wait for cooldown duration
+        yield return new WaitForSeconds(cooldownDuration);
+        // Reactivate myButton
+        myButton.interactable = true;
+    }
+}
